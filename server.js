bd6a9768bda7165a663f07e290f9d80aad036fc0
pancho1337 <pancho404x@gmail.com>
2020-10-21 20:27:41 -0500

    exercise app fcc

--- server.js
+++ server.js
@@ -1,3 +1,9 @@
+//TAKEN FROM GUIDE TO ALLOW DOTENV TO get data from .env
+// const dotenv = require('dotenv');
+// dotenv.config();
+require('dotenv').config();
+
+
 const express = require('express')
 const app = express()
 const bodyParser = require('body-parser')
@@ -5,13 +11,20 @@ const bodyParser = require('body-parser')
 const cors = require('cors')
 
 const mongoose = require('mongoose')
-mongoose.connect(process.env.MLAB_URI || 'mongodb://localhost/exercise-track' )
+
+// console.log(`Your port is ${process.env.PORT}`);
+console.log("all of env", process.env);
+const MongoClient = require('mongodb').MongoClient;
+const uri = process.env.MONGO_URI;
+const client = new MongoClient(uri, { useNewUrlParser: true });
+client.connect((err, data) => {
+  const collection = client.db("test").collection("devices");
+  // console.log('initail data obj', data)
+  // perform actions on the collection object
+  client.close();
+});
 
 app.use(cors())
-
-app.use(bodyParser.urlencoded({extended: false}))
-app.use(bodyParser.json())
-
 
 app.use(express.static('public'))
 app.get('/', (req, res) => {
@@ -19,30 +32,53 @@ app.get('/', (req, res) => {
 });
 
 
-// Not found middleware
-app.use((req, res, next) => {
-  return next({status: 404, message: 'not found'})
-})
-
-// Error Handling middleware
-app.use((err, req, res, next) => {
-  let errCode, errMessage
-
-  if (err.errors) {
-    // mongoose validation error
-    errCode = 400 // bad request
-    const keys = Object.keys(err.errors)
-    // report the first validation error
-    errMessage = err.errors[keys[0]].message
-  } else {
-    // generic or custom error
-    errCode = err.status || 500
-    errMessage = err.message || 'Internal Server Error'
-  }
-  res.status(errCode).type('txt')
-    .send(errMessage)
-})
-
 const listener = app.listen(process.env.PORT || 3000, () => {
   console.log('Your app is listening on port ' + listener.address().port)
 })
+
+let exeriseSessionSchema = new mongoose.Schema({
+  description: { type: String, requited: true },
+  duration: { type: Number, requited: true },
+  data: String
+})
+
+let userSchema = new mongoose.Schema({
+  username: { type: String, requited: true },
+  log: [exerciseSessionSchema]
+
+})
+
+let Session = mongoose.model('Session', exerciseSessionSchema)
+let User = mongoose.model('User', userSchema)
+
+
+
+// mongoose.connect(process.env.MLAB_URI || 'mongodb://localhost/exercise-track')
+
+
+// app.use(bodyParser.urlencoded({extended: false}))
+// app.use(bodyParser.json())
+
+// Not found middleware
+// app.use((req, res, next) => {
+//   return next({ status: 404, message: 'not found' })
+// })
+
+// // Error Handling middleware
+// app.use((err, req, res, next) => {
+//   let errCode, errMessage
+
+//   if (err.errors) {
+//     // mongoose validation error
+//     errCode = 400 // bad request
+//     const keys = Object.keys(err.errors)
+//     // report the first validation error
+//     errMessage = err.errors[keys[0]].message
+//   } else {
+//     // generic or custom error
+//     errCode = err.status || 500
+//     errMessage = err.message || 'Internal Server Error'
+//   }
+//   res.status(errCode).type('txt')
+//     .send(errMessage)
+// })
