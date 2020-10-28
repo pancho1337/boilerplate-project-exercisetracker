6e9b9e32a96efd2030492611f68e209e5f35c123
Francisco Rojas <pancho404x@gmail.com>
2020-10-27 21:01:11 -0600

    update to server.js
    update to server.js

--- server.js
+++ server.js
@@ -1,29 +1,14 @@
-//TAKEN FROM GUIDE TO ALLOW DOTENV TO get data from .env
-// const dotenv = require('dotenv');
-// dotenv.config();
-require('dotenv').config();
-
-
+// //TAKEN FROM GUIDE TO ALLOW DOTENV TO get data from .env
+// // const dotenv = require('dotenv');
+// // dotenv.config();
+// require('dotenv').config();
 const express = require('express')
 const app = express()
 const bodyParser = require('body-parser')
-
 const cors = require('cors')
-
 const mongoose = require('mongoose')
 
-// console.log(`Your port is ${process.env.PORT}`);
-console.log("all of env", process.env);
-const MongoClient = require('mongodb').MongoClient;
-const uri = process.env.MONGO_URI;
-const client = new MongoClient(uri, { useNewUrlParser: true });
-client.connect((err, data) => {
-  const collection = client.db("test").collection("devices");
-  // console.log('initail data obj', data)
-  // perform actions on the collection object
-  client.close();
-});
-
+mongoose.connect(uri, { useNewUrlParser: true, useUnifiedTopology: true });
 app.use(cors())
 
 app.use(express.static('public'))
@@ -31,54 +16,114 @@ app.get('/', (req, res) => {
   res.sendFile(__dirname + '/views/index.html')
 });
 
+const listener = app.listen(process.env.PORT || 3000,
+  () => {
+    console.log('Your app is listening on port ' + listener.address().port)
+  })
 
-const listener = app.listen(process.env.PORT || 3000, () => {
-  console.log('Your app is listening on port ' + listener.address().port)
-})
 
-let exeriseSessionSchema = new mongoose.Schema({
-  description: { type: String, requited: true },
-  duration: { type: Number, requited: true },
-  data: String
+let exerciseSessionSchema = new mongoose.Schema({
+  description: { type: String, required: true },
+  duration: { type: Number, required: true },
+  date: String
 })
 
 let userSchema = new mongoose.Schema({
-  username: { type: String, requited: true },
+  username: { type: String, required: true },
   log: [exerciseSessionSchema]
-
 })
 
 let Session = mongoose.model('Session', exerciseSessionSchema)
 let User = mongoose.model('User', userSchema)
 
+app.post('/api/exercise/new-user',
+  bodyParser.urlencoded({ extended: false }),
+  (request, response) => {
+    console.log(request.body)
+    let newUser = new User({ username: request.body.username })
+    newUser.save((error, savedUser) => {
+      if (error) {
+        console.error(error)
+      }
+      if (!error) {
+        let responseObject = {}
+        responseObject['username'] = savedUser.username
+        responseObject['_id'] = savedUser.id
+        response.json(responseObject)
+      }
+    })
+  })
 
+app.get('/api/exercise/users',
+  (request, response) => {
+    User.find({}, (error, arrayOfUsers) => {
+      console.log(arrayOfUsers)
+      if (!error) {
+        response.json(arrayOfUsers)
+      }
+    })
+  })
 
-// mongoose.connect(process.env.MLAB_URI || 'mongodb://localhost/exercise-track')
+app.post('/api/exercise/add',
+  bodyParser.urlencoded({ extended: false }),
+  (request, response) => {
+    let newSession = new Session({
+      description: request.body.description,
+      duration: parseInt(request.body.duration),
+      date: request.body.date
+    })
+    if (newSession.date === '') {
+      newSession.date = new Date().toISOString().substring(0, 10)
+    }
+    User.findByIdAndUpdate(request.body.userId,
+      { $push: { log: newSession } },
+      { new: true },
+      (error, updatedUser) => {
+        if (!error) {
+          let responseObject = {}
+          responseObject['_id'] = updatedUser.id
+          responseObject['username'] = updatedUser.username
+          responseObject['date'] = new Date(newSession.date).toDateString()
+          responseObject['description'] = newSession.description
+          responseObject['duration'] = newSession.duration
+          response.json(responseObject)
+        }
+      }
+    )
+  })
 
+app.get('/api/exercise/log', (request, response) => {
+  User.findById(request.query.userId, (error, result) => {
+    // if(!error){
+    //   let responseObject = result
+    //   responseObject['count'] = result.log.length
+    //   response.json(responseObject)
+    // }
+    if (!error) {
+      let responseObject = result
+      if (request.query.from || request.query.to) {
+        let fromDate = new Date(0)
+        let toDate = new Date()
+        if (request.query.from) {
+          fromDate = new Date(request.query.from)
+        }
+        if (request.query.to) {
+          toDate = new Date(request.query.to)
+        }
+        fromDate = fromDate.getTime()
+        toDate = toDate.getTime()
+        responseObject.log = responseObject.log.filter((session) => {
+          let sessionDate = new Date(session.date).getTime()
+          return sessionDate >= fromDate && sessionDate <= toDate
+        })
+      }
+      if (request.query.limit) {
+        responseObject.log = responseObject.log.slice(0, request.query.limit)
+      }
+      responseObject = responseObject.toJSON()
+      responseObject['count'] = result.log.length
+      response.json(responseObject)
+    }
+  })
 
-// app.use(bodyParser.urlencoded({extended: false}))
-// app.use(bodyParser.json())
-
-// Not found middleware
-// app.use((req, res, next) => {
-//   return next({ status: 404, message: 'not found' })
-// })
-
-// // Error Handling middleware
-// app.use((err, req, res, next) => {
-//   let errCode, errMessage
-
-//   if (err.errors) {
-//     // mongoose validation error
-//     errCode = 400 // bad request
-//     const keys = Object.keys(err.errors)
-//     // report the first validation error
-//     errMessage = err.errors[keys[0]].message
-//   } else {
-//     // generic or custom error
-//     errCode = err.status || 500
-//     errMessage = err.message || 'Internal Server Error'
-//   }
-//   res.status(errCode).type('txt')
-//     .send(errMessage)
-// })
+})
